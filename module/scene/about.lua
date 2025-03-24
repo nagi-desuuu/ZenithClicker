ac351efe62d4e80f5761f63d81b7ccd8c607083f
MrZ_26 <1046101471@qq.com>
2025-03-24 08:00:26 +0800

    修typo

--- module/scene/about.lua
+++ module/scene/about.lua
@@ -53,14 +53,14 @@ addText({ COLOR.LD, "Background Reconstruction" }, 180, 510, .2)
 
 addText({ clr.osk, "OSK" }, -320, 550, .5)
 addText({ COLOR.LD, "FOUNDER & LEAD PRODUCER..." }, -320, 590, .26)
-addText({ COLOR.LD, "of the Origial Game: TETR.IO" }, -320, 610, .2)
+addText({ COLOR.LD, "of the Original Game: TETR.IO" }, -320, 610, .2)
 
 addText({ clr.ocelot, "DOKTOROCELOT" }, 0, 550, .5)
 addText({ COLOR.LD, "AUDIO & MUSIC" }, 0, 590, .26)
 
 addText({ clr.garbo, "GARBO" }, 320, 550, .5)
 addText({ COLOR.LD, "GAME & WORLD DESIGN..." }, 320, 590, .26)
-addText({ COLOR.LD, "of the Origial Game: TETR.IO" }, 320, 610, .2)
+addText({ COLOR.LD, "of the Original Game: TETR.IO" }, 320, 610, .2)
 
 table.insert(lines, 640)
 
