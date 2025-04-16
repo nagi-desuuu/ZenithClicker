a96884ea7b64cc0f99e1f09309ad0a4df68bfab8
MrZ_26 <1046101471@qq.com>
2025-04-17 02:03:14 +0800

    # New - New mod icons for 1-mod and 2-mod run (by CreeperCraftYT) - Animation for in-game mod set - rNH+rIN: quest won't hide instantly in late game - rAS: now makes imperfect pass sends 0 attack - rEX: disables "+2 attack when combo contains DP" - rNH+AS: allow using x/c as mouse button # Fix - PB line shows at wrong position sometimes

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 010619,
-    ["verCode"] = 010619,
+    ["apkCode"] = 010620,
+    ["verCode"] = 010620,
 
-    ["appVer"] = "V1.6.19",
-    ["verStr"] = "1.6.19",
+    ["appVer"] = "V1.6.20",
+    ["verStr"] = "1.6.20",
 }
