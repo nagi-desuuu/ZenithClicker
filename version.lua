d66900508a7e41306865f8626874063b018c5a85
MrZ_26 <1046101471@qq.com>
2025-05-06 20:41:28 +0800

    Try official android build

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 010632,
-    ["verCode"] = 010632,
+    ["apkCode"] = 010633,
+    ["verCode"] = 010633,
 
-    ["appVer"] = "V1.6.32",
-    ["verStr"] = "1.6.32",
+    ["appVer"] = "V1.6.33",
+    ["verStr"] = "1.6.33",
 }
