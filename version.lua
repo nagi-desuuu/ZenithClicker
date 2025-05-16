2016002158e9664748cba9e056fd9a8188155c0d
MrZ_26 <1046101471@qq.com>
2025-05-17 07:24:20 +0800

    # New - Text revision - Better UI details # Fix - Some achievements broken - Several small bugs

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 010707,
-    ["verCode"] = 010707,
+    ["apkCode"] = 010708,
+    ["verCode"] = 010708,
 
-    ["appVer"] = "V1.7.7",
-    ["verStr"] = "1.7.7",
+    ["appVer"] = "V1.7.8",
+    ["verStr"] = "1.7.8",
 }
