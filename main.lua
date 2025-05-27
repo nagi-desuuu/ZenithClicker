ac38a895bbd23f5bd5470e0cdbc041bc158e4bcb
MrZ_26 <1046101471@qq.com>
2025-05-27 12:48:02 +0800

    修音乐调号标记

--- main.lua
+++ main.lua
@@ -805,7 +805,7 @@ BgmData = {
     f9r = { meta = '160 BPM  E Minor', bpm = 160, toneFix = -1, loop = { 36, 144 } },
     f10 = { meta = '100 BPM  C Major & A Minor', bpm = 100, toneFix = 2, loop = { 122.4, 232.8 } },
     f10r = { meta = '100 BPM  C Major & A Minor', bpm = 100, toneFix = 2, loop = { 122.4, 232.8 }, teleport = { 31.2, 98.4 } },
-    fomg = { meta = '90 & 100 BPM  C# Major & A# Minor', bpm = 100, toneFix = 3, loop = { 38.4 - 11.862, 144 - 11.862 }, end1 = 144 - 11.862, end2 = 153.6 - 11.862 },
+    fomg = { meta = '90 & 100 BPM  Db Major & Bb Minor', bpm = 100, toneFix = 3, loop = { 38.4 - 11.862, 144 - 11.862 }, end1 = 144 - 11.862, end2 = 153.6 - 11.862 },
     tera = { meta = '240 BPM  C# Minor', bpm = 240, toneFix = 1, loop = { 76, 140 }, introLen = 2, teleport = { -1, 20 }, end1 = 140, end2 = 142, end3 = 144, end4 = 146 },
     terar = { meta = '240 BPM  C# Minor', bpm = 240, toneFix = 1, loop = { 84 - 15.565, 172 - 15.565 }, teleport = { 0, 18 - 15.565 } },
 }
