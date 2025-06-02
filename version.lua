70f1f0f7d20313a300300d1336d59e7ea1027f9e
MrZ_26 <1046101471@qq.com>
2025-06-02 20:42:24 +0800

    # Fix - Some UI bugs - Add "cd" secret option - Several debugging features will be saved

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 010904,
-    ["verCode"] = 010904,
+    ["apkCode"] = 010905,
+    ["verCode"] = 010905,
 
-    ["appVer"] = "V1.9.4",
-    ["verStr"] = "1.9.4",
+    ["appVer"] = "V1.9.5",
+    ["verStr"] = "1.9.5",
 }
