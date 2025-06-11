406baa325763d89c46a0201a1d5ba8b789be257d
MrZ_26 <1046101471@qq.com>
2025-06-11 16:52:43 +0800

    补充制作人员名单

--- module/scene/ending.lua
+++ module/scene/ending.lua
@@ -39,6 +39,10 @@ addText("BG Reconstruction", -26, h, .42, 'right')
 addText("DJ Asriel", 26, h, .42, 'left')
 
 h = h + 95
+addText("Rank Icons", -26, h, .42, 'right')
+addText("Zyrixia", 26, h, .42, 'left')
+
+h = h + 95
 addText("Achievement Icons", -26, h, .42, 'right')
 addText("MrZ & DJ Asriel", 26, h, .42, 'left')
 
