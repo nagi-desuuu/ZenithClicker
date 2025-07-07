af3ef6df65cf6de2792280260209b3f5067f962a
MrZ_26 <1046101471@qq.com>
2025-07-08 03:38:58 +0800

    # Fix - GV & IN make bgm a bit glitchy - Remake card art - NH effect balancing - Remove Swamp Water Pro Plus

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011100,
-    ["verCode"] = 011100,
+    ["apkCode"] = 011101,
+    ["verCode"] = 011101,
 
-    ["appVer"] = "V1.11.0",
-    ["verStr"] = "1.11.0",
+    ["appVer"] = "V1.11.1",
+    ["verStr"] = "1.11.1",
 }
