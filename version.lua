48cc70eb0a1f957a1be7ed1a3b26a234049511b9
MrZ_26 <1046101471@qq.com>
2025-07-09 08:33:28 +0800

    # Fix - Remake card art again

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011101,
-    ["verCode"] = 011101,
+    ["apkCode"] = 011102,
+    ["verCode"] = 011102,
 
-    ["appVer"] = "V1.11.1",
-    ["verStr"] = "1.11.1",
+    ["appVer"] = "V1.11.2",
+    ["verStr"] = "1.11.2",
 }
