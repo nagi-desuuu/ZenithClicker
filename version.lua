67959db31eccc95707ba37a8ceee446b3ad707bf
MrZ_26 <1046101471@qq.com>
2025-09-20 10:15:38 +0800

    ## v1.12.7 Fix - Fix some text issues - Fix a card animation glitch

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011206,
-    ["verCode"] = 011206,
+    ["apkCode"] = 011207,
+    ["verCode"] = 011207,
 
-    ["appVer"] = "V1.12.6",
-    ["verStr"] = "1.12.6",
+    ["appVer"] = "V1.12.7",
+    ["verStr"] = "1.12.7",
 }
