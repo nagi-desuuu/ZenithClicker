5952734ffddc14d13481c8545898e7e7c34bfc7a
MrZ_26 <1046101471@qq.com>
2025-10-10 21:34:53 +0800

    # Change - Fix +3m on breaking floor barrier multiplied by rank - Fix PB line sometimes on wrong altitude - Fix font loading breaks RECORD page UI - New filtering options in RECORD page, adding more keyboard navigation - Unreveal a bit more hidden contents

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011301,
-    ["verCode"] = 011301,
+    ["apkCode"] = 011302,
+    ["verCode"] = 011302,
 
-    ["appVer"] = "V1.13.1",
-    ["verStr"] = "1.13.1",
+    ["appVer"] = "V1.13.2",
+    ["verStr"] = "1.13.2",
 }
