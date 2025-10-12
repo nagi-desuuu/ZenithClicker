762f1aa133921504114772c4a3389eeec0eaf972
MrZ_26 <1046101471@qq.com>
2025-10-12 23:19:16 +0800

    # Change - New text animation for DH & AS - Rework achievement Dazed - Fix some altitude rounding issues - Fix some UI issues - Add Some new secrets

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011303,
-    ["verCode"] = 011303,
+    ["apkCode"] = 011304,
+    ["verCode"] = 011304,
 
-    ["appVer"] = "V1.13.3",
-    ["verStr"] = "1.13.3",
+    ["appVer"] = "V1.13.4",
+    ["verStr"] = "1.13.4",
 }
