f100e599e33c1fac8a06a5fa9f4e5e8ddbd4d725
MrZ_26 <1046101471@qq.com>
2025-10-14 01:18:29 +0800

    彻底修成绩界面潜在问题，框架跟进

--- module/scene/records.lua
+++ module/scene/records.lua
@@ -65,7 +65,7 @@ local function newRecord(list, isUltra)
     local mods = #list == 0 and "No Mod" or table.concat(list, " ")
     table.sort(list)
     local setStr = (isUltra and 'u' or '') .. table.concat(list)
-    local height = rawget(BEST.highScore,setStr)
+    local height = rawget(BEST.highScore, setStr)
     if not height then return end
     local time = BEST.speedrun[setStr]
     local floor = height >= 6200 and 11 or GAME.calculateFloor(height)
@@ -242,7 +242,8 @@ local function query()
                     if notMatch then break end
                 end
 
-                table.insert(recList, newRecord(setL, ultra))
+                local rec = newRecord(setL, ultra)
+                if rec then table.insert(recList, rec) end
             until true
         end
         table.sort(recList,
