783f082156a9c103e94103beb30c976625fa2033
MrZ_26 <1046101471@qq.com>
2025-10-13 10:10:17 +0800

    # Fix - Some texts - A UI bug - A possible UI bug - Add a new badge

--- version.lua
+++ version.lua
@@ -1,8 +1,8 @@
 return {
     ["appName"] = "Zenith Clicker",
-    ["apkCode"] = 011304,
-    ["verCode"] = 011304,
+    ["apkCode"] = 011305,
+    ["verCode"] = 011305,
 
-    ["appVer"] = "V1.13.4",
-    ["verStr"] = "1.13.4",
+    ["appVer"] = "V1.13.5",
+    ["verStr"] = "1.13.5",
 }
